beac1eb9bbbc7c5fc7b056cff628a16e084b6c50
Ahmet <107392036+ahmetcengiz07@users.noreply.github.com>
2023-01-16 19:04:18 +0300

    init Test Project

---
+++ App.js
@@ -0,0 +1,12 @@
+import {SafeAreaView, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Test with Jest</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
